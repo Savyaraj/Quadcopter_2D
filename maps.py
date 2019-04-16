f5521b4314f0b8527f53e62383b7db3736335192
Savyaraj <savya.deshmukh@gmail.com>
2019-04-17 04:12:03 +0530

    Update maps.py
    Adding obstacles to the map
    Collision checking
    More user oriented map generation

--- maps.py
+++ maps.py
@@ -1,15 +1,22 @@
 # 2-D maps for trajectory generation
+import numpy as np 
+
 
 class free_map():
-	def __init__(size, start, goal):
-		self.map_size = size
-		self.start = start
-		self.goal = goal
+	def __init__(self,size,start,goal):
+		self.map_size = np.array(size)
+		self.start = np.array(start)
+		self.goal = np.array(goal)
 
 
 class obstacle_map():
-	def __init__(size, start, goal):
-		self.map_size = size
-		self.start = start
-		self.goal = goal
+	def __init__(size,start,goal):
+		self.map_size = np.array(size)
+		self.start = np.array(start)
+		self.goal = np.array(goal)
+		self.obstacles = []                      #obstacle array
+	def add_rectangle(self,x,y,l,w):
 
+	def add_circle(self,x,y,r):
+
+	def is_colliding(self,x,y,theta):
