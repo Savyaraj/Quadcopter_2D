add8388b373fe95ddd50f9c2d180a788b6a28fa5
Savyaraj <savya.deshmukh@gmail.com>
2019-04-17 04:55:13 +0530

    Update maps.py
    Add circle collision avoidance

--- maps.py
+++ maps.py
@@ -1,5 +1,6 @@
 # 2-D maps for trajectory generation
 import numpy as np 
+import math
 
 
 class free_map():
@@ -14,9 +15,24 @@ class obstacle_map():
 		self.map_size = np.array(size)
 		self.start = np.array(start)
 		self.goal = np.array(goal)
-		self.obstacles = []                      #obstacle array
-	def add_rectangle(self,x,y,l,w):
+		self.obstacles = {}                      #obstacle array
+
+	def add_rectangle(self,x,y,l,w):             # (x,y): center ,  (l,w): length,width
+		self.obstacles["Rectangle"].append([x,y,l,w])
+		return
 
 	def add_circle(self,x,y,r):
+		self.obstacles["Cicle"].append([x,y,r])
+		return
 
-	def is_colliding(self,x,y,theta):
+	def is_colliding(self,x,y,L,theta):
+
+		r = [x+(L/2)*math.cos(theta), y-(L/2)*math.sin(theta)]
+		l = [x-(L/2)*math.cos(theta), y+(L/2)*math.sin(theta)]
+
+		for R in self.obstacles['Rectangle']:
+			if(((R[0]-R[2]/2)<=r[0] and (r[0]<=R[0]+R[2]/2)) or ((R[0]-R[2]/2)<=l[0] and (l[0]<=R[0]+R[2]/2))):
+				return True
+
+			if(((R[1]-R[3]/2)<=r[1] and (r[1]<=R[1]+R[3]/2)) or ((R[1]-R[3]/2)<=l[1] and (l[1]<=R[1]+R[3]/2))):
+				return True	
