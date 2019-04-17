fa22b684188c99d86e43b04d9833829c0349f26f
Savyaraj <savya.deshmukh@gmail.com>
2019-04-18 04:01:50 +0530

    Update main.py

--- main.py
+++ main.py
@@ -8,26 +8,28 @@ import math
 from maps import *
 from TrajectoryGen import *
 
-map1 = obstacle_map([-30,30],[-20,-20],[25,10])
-map1.add_rectangle(-17,-20,20,20)
+map1 = obstacle_map([-5,5],[-2,-2],[3,4])
+#map1.add_rectangle(0	,-20,20,20)
 v = Renderer(map1)
-# v.draw_point(map1.start,'b',5)
-# v.draw_point(map1.goal,'g',5)
-# plt.pause(1)
-g = graph(3,map1,v,[30,7,10,50],[0.2,5],10000)
+v.draw_point(map1.start,'b',5)
+v.draw_point(map1.goal,'g',5)
+plt.pause(1)
+g = graph(2,map1,v,[5,5,10,50],[0.1,8],10000)
 
 # list = g.neighbors(g.start)
 # print(list[0].p)
 # print(g.cost(g.start,list[0]))
 # print(g.heuristic(g.start,list[0]))
 
-a,b,path = g.A_star_search(g.start,g.goal)
-# plt.pause(10)
+came_from,b,path = g.A_star_search(g.start,g.goal)
 
-# path.remove(None)
-# x = []
-# y = []
-# for current in path:
-# 	x = np.append(x,current[0])
-# 	y = np.append(y,current[1])
-# print(path)
+for current in came_from:
+	print(a[current])
+	prev = came_from[current.open()]
+	v.draw_point(current.p[0],'r',1)
+	theta = math.atan((current.p[0][1]-prev.p[0][1])/(current.p[0][0]-prev.p[0][0]))
+	vel = np.linalg.norm(current.p[1])
+	print(vel)
+	temp = current.p[0]+[-(vel/g.v_max)*math.sin(theta),(vel/g.v_max)*math.cos(theta)]
+	v.draw_line(current.p[0],temp,'b')
+plt.pause(10)
