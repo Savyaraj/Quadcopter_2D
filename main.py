c4852c040759ea5b4b81bf4661282148f7a61abb
Savyaraj <savya.deshmukh@gmail.com>
2019-04-18 04:13:47 +0530

    Update main.py

--- main.py
+++ main.py
@@ -14,18 +14,21 @@ v = Renderer(map1)
 v.draw_point(map1.start,'b',5)
 v.draw_point(map1.goal,'g',5)
 plt.pause(1)
-g = graph(2,map1,v,[5,5,10,50],[0.1,8],10000)
+dim = 2
+lim = [5,7,10,50]
+rho = 4*lim[dim]**2
+g = graph(dim,map1,v,lim,[0.2,5],rho)
 
 # list = g.neighbors(g.start)
 # print(list[0].p)
 # print(g.cost(g.start,list[0]))
 # print(g.heuristic(g.start,list[0]))
 
-came_from,b,path = g.A_star_search(g.start,g.goal)
+came_from,cost_so_far = g.A_star_search(g.start,g.goal)
 
 for current in came_from:
-	print(a[current])
-	prev = came_from[current.open()]
+	print(came_from[current])
+	prev = came_from[current]
 	v.draw_point(current.p[0],'r',1)
 	theta = math.atan((current.p[0][1]-prev.p[0][1])/(current.p[0][0]-prev.p[0][0]))
 	vel = np.linalg.norm(current.p[1])
