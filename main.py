bbd5ed8c4de704126e77208b3c812a8ade5cc690
Savyaraj <savya.deshmukh@gmail.com>
2019-04-22 02:57:28 +0530

    Update main.py

--- main.py
+++ main.py
@@ -8,15 +8,15 @@ import math
 from maps import *
 from TrajectoryGen import *
 
-map1 = obstacle_map([-6,6],[-4,-4],[4,4])
-map1.add_rectangle(-3,-3,4,4)
-# map1.add_rectangle(0,1,2,4)
+map1 = obstacle_map([-8,8],[-6,-6],[7,-7])
+map1.add_rectangle(-4,-8,1,5)
+map1.add_rectangle(3,-8,1,3)
 v = Renderer(map1)
 dim = 1
-lim = [5,7,8,20]
+lim = [8,7,7,20]
 rho = 15*lim[dim]**2        # rho = 4 for vel control
 	 					   #       2 for acc control
-resolution = [0.2,7]
+resolution = [0.2,6]
 heuristic_method = 'distance'
 iterations = 100000
 g = graph(dim,map1,v,lim,resolution,rho,[])    #5 bins for acc, 8 for vel
