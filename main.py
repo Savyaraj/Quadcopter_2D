c1d65008a34ae25ecb2ee16cf6658f8a87cba8a0
Savyaraj <savya.deshmukh@gmail.com>
2019-04-22 02:06:30 +0530

    Update main.py

--- main.py
+++ main.py
@@ -8,44 +8,36 @@ import math
 from maps import *
 from TrajectoryGen import *
 
-map1 = obstacle_map([-5,5],[-4,-4],[4,-4])
-map1.add_rectangle(-3,-5,3,4)
-map1.add_rectangle(0,1,2,2)
+map1 = obstacle_map([-6,6],[-4,-4],[4,4])
+map1.add_rectangle(-3,-3,4,4)
+# map1.add_rectangle(0,1,2,4)
 v = Renderer(map1)
-v.draw_point(map1.start,'b',5)
-v.draw_point(map1.goal,'g',5)
 dim = 1
-lim = [5,5,10,50]
-rho = 4*lim[dim]**2        # rho = 4 for vel control
+lim = [5,7,8,20]
+rho = 15*lim[dim]**2        # rho = 4 for vel control
 	 					   #       2 for acc control
+resolution = [0.2,7]
+heuristic_method = 'distance'
+iterations = 100000
+g = graph(dim,map1,v,lim,resolution,rho,[])    #5 bins for acc, 8 for vel
 
-g = graph(dim,map1,v,lim,[0.2,8],rho)    #5 bins for acc, 8 for vel
-
-# list = g.neighbors(g.start)
-# print(list[0].p)
-# print(g.cost(g.start,list[0]))
-# print(g.heuristic(g.start,list[0]))
-
-came_from,cost_so_far,current = g.A_star_search(g.start,g.goal)
-g.write_data()
+came_from,cost_so_far,current = g.A_star_search(g.start,g.goal,iterations)
 v.close()
 v = Renderer(map1)
-v.draw_point(map1.start,'b',5)
-v.draw_point(map1.goal,'g',5)
+g.plot_trajectory()
 
-# g = graph(dim,map1,v,lim,[0.2,4],rho)
-# g.read_data("came_from.txt")
-# came_from = g.camefrom 
-# current = g.last
+rho = 15*lim[dim+1]**2        # rho = 4 for vel control
+	 					   #       2 for acc control
+resolution = [0.2,6]
+g1 = graph(dim+1,map1,v,lim,resolution,rho,g.read_data('Trajectory.txt'))
+came_from,cost_so_far,current = g1.A_star_search(g1.start,g1.goal,iterations)
+v.close()
+v = Renderer(map1)
+g1.plot_trajectory()
 
-while came_from[current.open()] != None:
-	prev = came_from[current.open()]
-	v.draw_line(current.p[0],prev.p[0],'r')
-	if current == None or prev == None:
-		continue
-	theta = math.atan((current.p[0][1]-prev.p[0][1])/(current.p[0][0]-prev.p[0][0]))
-	# vel = np.linalg.norm(current.p[1])
-	# temp = current.p[0]+[-(vel/g.v_max)*math.sin(theta),(vel/g.v_max)*math.cos(theta)]
-	# v.draw_line(current.p[0],temp,'b')
-	current = prev
-plt.pause(10)
+rho = 15*lim[dim+2]**2
+g2 = graph(dim+2,map1,v,lim,resolution,rho,g1.read_data('Trajectory.txt'))#,g.read_data('Trajectory.txt'))
+came_from,cost_so_far,current = g2.A_star_search(g2.start,g2.goal,iterations)
+v.close()
+v = Renderer(map1)
+g2.plot_trajectory()
