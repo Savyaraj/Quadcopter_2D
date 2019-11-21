9c577a1b1e9043e1e7027ad7312018f67c5cf0c9
Savyaraj <savya.deshmukh@gmail.com>
2019-11-21 11:36:29 +0100

    Update A_star_redblob.py

--- A_star_redblob.py
+++ A_star_redblob.py
@@ -94,7 +94,7 @@ class GridWithWeights(SquareGrid):
         return 1#self.weights.get(to_node, 1)
 
 diagram4 = GridWithWeights(10, 10)
-diagram4.walls = [(2,2),(3,2),(4, 2),(6, 2),(7,2),(8,2),(2,4),(3,4),(4, 4),(6, 4),(7,4),(8,4),(2,6),(3,6),(4, 6),(6, 6),(7,6),(8,6),(2,8),(3,8),(4, 8),(6, 8),(7,8),(8,8)]
+diagram4.walls = [(4,7),(4,5),(2,1),(2,2),(3,2),(4, 2),(6, 2),(7,2),(8,2),(2,4),(3,4),(4, 4),(6, 4),(7,4),(8,4),(2,6),(3,6),(4, 6),(6, 6),(7,6),(8,6),(2,8),(3,8),(4, 8),(6, 8),(7,8),(8,8)]
 diagram4.weights = {loc: 5 for loc in [(3, 4), (3, 5), (4, 1), (4, 2),
                                        (4, 3), (4, 4), (4, 5), (4, 6), 
                                        (4, 7), (4, 8), (5, 1), (5, 2),
@@ -185,7 +185,7 @@ def a_star_search(graph, start, goal):
     return came_from, cost_so_far
 
 
-start, goal = (1, 1), (7,5)
+start, goal = (1, 1), (7,7)
 came_from, cost_so_far = a_star_search(diagram4, start, goal)
 draw_grid(diagram4, width=3, point_to=came_from, start=start, goal=goal)
 print()
