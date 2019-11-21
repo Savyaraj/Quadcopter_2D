78a0ecc5c4c83cc067f0ccd4995689e9c4f6d405
Savyaraj <savya.deshmukh@gmail.com>
2019-11-21 11:30:05 +0100

    Update A_star_redblob.py

--- A_star_redblob.py
+++ A_star_redblob.py
@@ -62,7 +62,7 @@ def draw_grid(graph, width=2, **style):
         print()
 
 # data from main article
-DIAGRAM1_WALLS = [from_id_width(id, width=30) for id in [21,22,51,52,81,82,93,94,111,112,123,124,133,134,141,142,153,154,163,164,171,172,173,174,175,183,184,193,194,201,202,203,204,205,213,214,223,224,243,244,253,254,273,274,283,284,303,304,313,314,333,334,343,344,373,374,403,404,433,434]]
+DIAGRAM1_WALLS = [from_id_width(id, width=30) for id in [5,6,7,8,9,10,21,22,51,52,81,82,93,94,111,112,123,124,133,134,141,142,153,154,163,164,171,172,173,174,175,183,184,193,194,201,202,203,204,205,213,214,223,224,243,244,253,254,273,274,283,284,303,304,313,314,333,334,343,344,373,374,403,404,433,434]]
 
 class SquareGrid:
     def __init__(self, width, height):
@@ -91,10 +91,10 @@ class GridWithWeights(SquareGrid):
         self.weights = {}
     
     def cost(self, from_node, to_node):
-        return self.weights.get(to_node, 1)
+        return 1#self.weights.get(to_node, 1)
 
 diagram4 = GridWithWeights(10, 10)
-diagram4.walls = [(1, 7), (1, 8), (2, 7), (2, 8), (3, 7), (3, 8)]
+diagram4.walls = [(2,2),(3,2),(4, 2),(6, 2),(7,2),(8,2),(2,4),(3,4),(4, 4),(6, 4),(7,4),(8,4),(2,6),(3,6),(4, 6),(6, 6),(7,6),(8,6),(2,8),(3,8),(4, 8),(6, 8),(7,8),(8,8)]
 diagram4.weights = {loc: 5 for loc in [(3, 4), (3, 5), (4, 1), (4, 2),
                                        (4, 3), (4, 4), (4, 5), (4, 6), 
                                        (4, 7), (4, 8), (5, 1), (5, 2),
@@ -185,7 +185,7 @@ def a_star_search(graph, start, goal):
     return came_from, cost_so_far
 
 
-start, goal = (1, 4), (7, 8)
+start, goal = (1, 1), (7,5)
 came_from, cost_so_far = a_star_search(diagram4, start, goal)
 draw_grid(diagram4, width=3, point_to=came_from, start=start, goal=goal)
 print()
